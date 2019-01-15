0d06e4f76f46e12763827e2a17276113f1b67673
M.HLU <myriam_el_helou@hotmail.com>
2019-01-15 11:05:59 +0100

    Update TEI2JSON.py

--- TEI2JSON.py
+++ TEI2JSON.py
@@ -42,6 +42,7 @@ def create_json(rng_file):
             list_doc = []
             #récupération des attributs externes
             for att in link.find_all('ref'):
+                attribute2 = OrderedDict()
                 if att:
                     attributs = att.get('name')
                     if str(attributs).startswith("tei_att"):
@@ -71,15 +72,32 @@ def create_json(rng_file):
                                                                 if attr:
                                                                     name_attr = attr.get('name')
                                                                     list_name.append(name_attr)
+                                                                    choice = attr.find('choice')
+                                                                    liste_values = []
+                                                                    if choice:
+                                                                        type2 = 'Enumerated'
+                                                                        for value in choice.find_all('value'):
+                                                                            liste_values.append(value.string)
+                                                                    else:
+                                                                        type2 = 'String'
+                                                                        liste_values.append('NONE')
                                                                     documentation = attr.find('a:documentation')
                                                                     if documentation:
+                                                                        attribute2['key'] = name_attr
+                                                                        attribute2['type'] = type2
+                                                                        attribute2['required'] = False
+                                                                        attribute2['documentation'] = documentation.string
                                                                         list_doc.append(documentation.string)
+                                                                        attribute2['value'] = liste_values
+
+                                                            print(attribute2)
+                                                            #element['attributs'].append(attribute2)
                                                 list_ref.append(ref_name)
             print(name)
-            print(list_attributs)
-            print(list_ref)
-            print(list_name)
-            print(list_doc)
+            #print(list_attributs)
+            # print(list_ref)
+            #print(list_name)
+            #print(list_doc)
             #récupération des attributs qui se trouvent dans l'élement
             for attribut in link.find_all('attribute'):
                 attribute = OrderedDict()
