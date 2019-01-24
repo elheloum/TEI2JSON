85b007b08a896979adf35eacccb5aa43f1cf311b
elheloum <myriam_el_helou@hotmail.com>
2019-01-24 13:01:37 +0100

    VERSION 4

--- TEI2JSON.py
+++ TEI2JSON.py
@@ -11,7 +11,39 @@ from collections import OrderedDict
 import sys
 
 
+def file_tag(rng_file):
+    """
+    Ceci est l'étape 1 du traitement ! à lancer impérativement seule et en premier !!
+    :param rng_file:
+    :return:
+    """
+    file = open(rng_file, mode='r', encoding='UTF-8')
+    xml = file.read()
+    file.close()
+    file_out = open("element.txt", mode='w', encoding='UTF-8')
+    soup = BeautifulSoup(xml, 'xml')
+    for link in soup.find_all('element'):
+        # récupération du nom de l'élément
+        name = link.get('name')
+        if name:
+            # écrire les éléments dans un fichier de sortie
+            file_out.write(name + "\n")
+    file_out.close()
+
+
 def create_json(rng_file):
+    """
+    Ceci est l'étape 2 du traitement. A lancer impérativement après l'étape 1 !!
+    :param rng_file:
+    :return:
+    """
+    # création de  liste qui contiendra tous les tag trouvé dans le fichier .rng
+    file_in = open("element.txt", mode='r', encoding='UTF-8')
+    element = file_in.read()
+    elements = element.split("\n")
+    file_in.close()
+
+    # début du traitement du rng
     file = open(rng_file, mode='r', encoding='UTF-8')
     xml = file.read()
     file.close()
@@ -280,5 +312,12 @@ def create_json(rng_file):
 
 
 if __name__ == '__main__':
-    create_json("myTEI-3.rng")
+    """
+        Etape 1 
+    """
+    file_tag("myTEI-3.rng")
+    """
+        Etape 2 
+    """
+    # create_json("myTEI-3.rng")
     # create_json(sys.argv[1])
